dfcfba66ccd8bf23f674494ec7cf623e65e078bf
SourceH <zizark4040@gmail.com>
2023-02-28 09:11:24 +0900

    mobile menu update

--- src/pages/layout.js
+++ src/pages/layout.js
@@ -1,9 +1,27 @@
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { useState, useEffect } from 'react'
 import { FaTimes, FaBars } from 'react-icons/fa'
 
 export default function Layout({ children }) {
   const router = useRouter()
+
+  {
+    /* Mobile Menu */
+  }
+  const [mobileMenu, setMobileMenu] = useState(false)
+
+  const handleMobileMenu = () => {
+    setMobileMenu(!mobileMenu)
+  }
+
+  useEffect(() => {
+    if (mobileMenu) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = 'unset'
+    }
+  }, [mobileMenu])
 
   return (
     <>
@@ -27,11 +45,46 @@ export default function Layout({ children }) {
 
         <div className="flex items-center">
           <button className="font-semibold text-lg hidden mobile:block">로그인</button>
-          <button className="font-semibold text-3xl mr-4 text-gray-500 transition-all hover:text-[#f1f1f1] mobile:hidden">
+          <button
+            className="font-semibold text-3xl text-gray-500 transition-all hover:text-[#f1f1f1] mobile:hidden"
+            onClick={handleMobileMenu}
+          >
             <FaBars />
-          </button>  
+          </button>
         </div>
 
+        {/* Mobile Menu */}
+        <div
+          className={`fixed top-0 left-0 w-full h-full bg-[#141920] z-50 transition-all mobile:hidden ${
+            mobileMenu ? 'opacity-100' : 'opacity-0 pointer-events-none'
+          }`}
+        >
+          <div className="flex justify-end items-center h-20 w-full">
+            <button
+              className="font-semibold text-3xl mr-4 text-gray-500 transition-all hover:text-[#f1f1f1]"
+              onClick={handleMobileMenu}
+            >
+              <FaTimes />
+            </button>
+          </div>
+          <div className="flex flex-col justify-start items-start h-full w-full px-4">
+            <button
+              className="font-semibold text-2xl mb-4 text-gray-500 transition-all hover:text-[#f1f1f1]"
+              onClick={() => router.push('/')}
+            >
+              홈
+            </button>
+            <button className="font-semibold text-2xl mb-4 text-gray-500 transition-all hover:text-[#f1f1f1]">
+              공지사항
+            </button>
+            <button className="font-semibold text-2xl mb-4 text-gray-500 transition-all hover:text-[#f1f1f1]">
+              추가하기
+            </button>
+            <button className="font-semibold text-2xl mb-4 text-gray-500 transition-all hover:text-[#f1f1f1]">
+              로그인
+            </button>
+          </div>
+        </div>
       </header>
       {children}
       <footer className="flex justify-center items-center h-20 w-full">
