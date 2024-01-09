96c1b1dc86fe65f54b6febe31ded61887cf86325
SourceH <zizark4040@gmail.com>
2024-01-09 13:35:21 +0900

    1.0.6.6
    구글 애널리틱스

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,9 +1,17 @@
-import '@/styles/globals.css'
-import Layout from './components/layout'
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { SessionProvider } from 'next-auth/react'
+import '@/styles/globals.css';
+import React from 'react';
+import Layout from './components/layout';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SessionProvider } from 'next-auth/react';
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize(process.env.NEXT_PUBLIC_GA_ID)
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  React.useEffect(() => {
+    ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
+  }, []);
+
   return (
     <>
       <SpeedInsights />
@@ -13,5 +21,5 @@ export default function App({ Component, pageProps: { session, ...pageProps } })
         </Layout>
       </SessionProvider>
     </>
-  )
+  );
 }
