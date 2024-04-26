a210a4e54d01171fae9360ec25402492272e2552
SourceH <zizark4040@gmail.com>
2024-04-26 10:07:17 +0900

    1.2.1
    폰트 로드 최적화

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,4 +1,5 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
 import React from 'react'
 import Layout from './components/layout'
 import { SpeedInsights } from '@vercel/speed-insights/next'
@@ -17,6 +18,10 @@ export default function App({
 
   return (
     <>
+      <Head>
+        {/* eslint-disable-next-line @next/next/no-page-custom-font */}
+        <link href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap' rel='stylesheet' />
+      </Head>
       <SpeedInsights />
       <SessionProvider session={session}>
         <Layout>
