3396b49272ee0ec6b7b9eedf25aa56c3f52cdb1b
SourceH <zizark4040@gmail.com>
2024-04-26 10:15:44 +0900

    1.2.1

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -19,8 +19,13 @@ export default function App({
   return (
     <>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         {/* eslint-disable-next-line @next/next/no-page-custom-font */}
-        <link href='https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap' rel='stylesheet' />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <SpeedInsights />
       <SessionProvider session={session}>
